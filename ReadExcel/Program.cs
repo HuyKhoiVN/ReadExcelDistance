716440c689cf33e415aa21bc32617bff3cfa2612
PhuNL4 <PhuNL4@fpt.com>
2025-03-07 23:50:53 +0700

    fix program

--- ReadExcel/Program.cs
+++ ReadExcel/Program.cs
@@ -1,11 +1,23 @@
+using ReadExcel.Repositories;
+using ReadExcel.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "ReadExcel", Version = "v1" });
+});
+builder.Services.AddHttpClient();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IRepairPersonRepository, RepairPersonRepository>();
+builder.Services.AddTransient<IAssignmentService, AssignmentService>();
+builder.Services.AddTransient<IOptimizationService, OptimizationService>();
+builder.Services.AddTransient<IExportService, ExportService>();
 
 var app = builder.Build();
 
