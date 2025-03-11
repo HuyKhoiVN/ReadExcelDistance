791ce875cd12dd29d9b32ca0fb4d8aea5563ab4a
HuyKhoiVN <huykhoi210@gmail.com>
2025-03-11 10:18:25 +0700

    add dtos

---
+++ ReadExcelProcess/DTO/DistanceMatrix.cs
@@ -0,0 +1,8 @@
+﻿namespace ReadExcelProcess.DTO
+{
+    public class DistanceMatrix
+    {
+        public float Duration { get; set; }
+        public float Distance { get; set; }
+    }
+}
---
+++ ReadExcelProcess/DTO/Location.cs
@@ -0,0 +1,9 @@
+﻿namespace ReadExcelProcess.DTO
+{
+    public class Location
+    {
+        public string Address { get; set; }
+        public decimal Latitute { get; set; }
+        public decimal Longitude { get; set; }
+    }
+}
